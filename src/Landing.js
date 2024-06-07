37c3b40cc5bfd0f50033c0159a51be2b2bc0ff21
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-06-07 15:51:45 +1000

    Started Landing page, completed Hero

---
+++ src/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Landing.css'
+
+export default function Landing() {
+    return (
+        <div className="Landing">
+            <h1>
+                Shot Box
+                <br />- Cafe -
+            </h1>
+        </div>
+    )
+}
